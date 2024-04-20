fc118f41851b9ef43fcfcd3291576667e6018b21
Tereza Kalatova <kalatovatereza@gmail.com>
2024-04-20 12:19:17 +0200

    úprava zápisu nadpisu do 1 řádku

--- Task_4.sql
+++ Task_4.sql
@@ -1,5 +1,4 @@
-/* OTÁZKA 4: Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd 
- (větší než 10 %)? */
+-- OTÁZKA 4: Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
 
 SELECT DISTINCT 
 	tkpf.pay_year AS previous_year,
